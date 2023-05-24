eff98863839d5afb597031c69a100e0946818bcf
Ahmad Yani <98377874+Ayye1011@users.noreply.github.com>
2023-05-24 06:47:34 +0000

    membuat program untuk menentukan apakah bilangan yang diinput oleh user merupakan angka dari kelipatan 7 atau bukan

--- Tugas-Mandiri-2/kelipatan7.go
+++ Tugas-Mandiri-2/kelipatan7.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+)
+
+func isMultipleOfSeven(n int) bool {
+	if n%7 == 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	// Menerima input dari pengguna
+	var num int
+	fmt.Print("Masukkan sebuah bilangan: ")
+	fmt.Scanln(&num)
+
+	// Memeriksa apakah bilangan merupakan kelipatan 7 atau bukan
+	if isMultipleOfSeven(num) {
+		fmt.Printf("%d adalah kelipatan dari 7.\n", num)
+	} else {
+		fmt.Printf("%d bukan kelipatan dari 7.\n", num)
+	}
+}
