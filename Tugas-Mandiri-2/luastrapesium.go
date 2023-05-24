6aee92176791bf77a7bcfaf049a688a5043b1182
Ahmad Yani <98377874+Ayye1011@users.noreply.github.com>
2023-05-24 06:50:43 +0000

    membuat program untuk menghitung luas trapesium pada file luastrapesium.go

--- Tugas-Mandiri-2/luastrapesium.go
+++ Tugas-Mandiri-2/luastrapesium.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+)
+
+func calculateArea(a float64, b float64, h float64) float64 {
+	// Menghitung luas trapesium menggunakan rumus: (a + b) * h / 2
+	area := (a + b) * h / 2
+	return area
+}
+
+func main() {
+	// Menerima input dari pengguna
+	var base1, base2, height float64
+	fmt.Println("Program Menghitung Luas Trapesium")
+	fmt.Print("Masukkan panjang alas atas: ")
+	fmt.Scanln(&base1)
+	fmt.Print("Masukkan panjang alas bawah: ")
+	fmt.Scanln(&base2)
+	fmt.Print("Masukkan tinggi trapesium: ")
+	fmt.Scanln(&height)
+
+	// Menghitung luas trapesium
+	area := calculateArea(base1, base2, height)
+
+	// Menampilkan hasil
+	fmt.Printf("Luas trapesium: %.2f\n", area)
+}
