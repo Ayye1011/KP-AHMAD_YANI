b1e3f6c8716707dbeb988909c5e02bc8c3fe1f2e
Ahmad Yani <98377874+Ayye1011@users.noreply.github.com>
2023-05-24 06:45:45 +0000

    membuat program untuk menentukan apakah bilang yang di tanya adalah bilang prima atau bukan pada file prima.go

--- Tugas-Mandiri-2/prima.go
+++ Tugas-Mandiri-2/prima.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+)
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	// Mengecek apakah bilangan n habis dibagi oleh bilangan dari 2 hingga akar(n)
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	// Menerima input dari pengguna
+	var num int
+	fmt.Print("Masukkan sebuah bilangan: ")
+	fmt.Scanln(&num)
+
+	// Memeriksa apakah bilangan merupakan bilangan prima atau bukan
+	if isPrime(num) {
+		fmt.Printf("%d adalah bilangan prima.\n", num)
+	} else {
+		fmt.Printf("%d bukan bilangan prima.\n", num)
+	}
+}
